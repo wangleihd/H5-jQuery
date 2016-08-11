3f086bd04cef18e4df14e64e22191278f533b02b
Richard Wang <wangleihd@gmail.com>
2016-08-11 15:33:03 +0800

    update dollar 1.1

--- class-002/js/dollar.js
+++ class-002/js/dollar.js
@@ -4,10 +4,16 @@ var $ = jQuery = function(){
 
 jQuery.fn = jQuery.prototype = {
     init: function(){
+        this.length = 0;
+        this.test = function(){
+            return this.length;
+        }
         return this;
     },
     ver: "1.0",
+    length: 1,
     size: function(){
         return this.length;
     }
 }
+jQuery.fn.init.prototype = jQuery.fn;
