c0ebf72050ba01706d18f391ab9cbdcef690da81
Richard Wang <wangleihd@gmail.com>
2016-08-11 16:54:08 +0800

    update dollar 1.2

--- class-002/js/dollar.js
+++ class-002/js/dollar.js
@@ -1,17 +1,33 @@
-var $ = jQuery = function(){
-    return  jQuery.fn.init();
+var $ = jQuery = function(selector, context){
+    return  jQuery.fn.init(selector, context);
 }
 
 jQuery.fn = jQuery.prototype = {
-    init: function(){
-        this.length = 0;
-        this.test = function(){
-            return this.length;
+    init: function(selector, context){
+        selector = selector || document;
+        context = context || document;
+        if( selector.nodeType ){
+            this[0] = selector;
+            this.length = 1;
+            this.context = selector;
+            return this;
+        }
+        if( typeof selector === "string"){
+            var e = context.getElementsByTagName(selector);
+            for(var i = 0; i < e.length; i++){
+                this[i] = e[i];
+            }
+            this.length = e.length;
+            this.context = context;
+            return this;
+        }else{
+            this.length = 0;
+            this.context = context;
+            return this;
         }
         return this;
     },
     ver: "1.0",
-    length: 1,
     size: function(){
         return this.length;
     }
